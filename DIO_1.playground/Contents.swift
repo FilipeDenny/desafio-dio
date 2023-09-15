d58b7f9cdac2093054f7cd38dc52b9adea47e588
Filipe Denny <filipedenny@Mac-mini-de-Filipe.local>
2023-09-15 13:32:01 -0300

    Desafio Dio

---
+++ DIO_1.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let founder = "Steve"
+var name = "Jobs"
+let cofounder = "Wozniak"
+
+let apple = "O fundador da apple é o \(founder + " " + name), mas seu cofundador é \(founder) \(cofounder) "
+
+print(apple)
